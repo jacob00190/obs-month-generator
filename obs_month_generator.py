7cccf94054d22f57d79264162b2f5233e183c095
Jacob Schlosser <jschlosser22@my.whitworth.edu>
2025-07-16 22:40:57 -0700

    Created obs_month_generator.py

---
+++ obs_month_generator.py
@@ -0,0 +1,8 @@
+#######################################################################
+# Name: Jacob Schlosser
+# Program: obs_month_generator
+# Purpose: To generate a markdown (.md) file formatted to have every
+#          day of a given month with various sections for journalling.
+#######################################################################
+
+import datetime
